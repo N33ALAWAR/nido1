c2ce1ad3a716f1f3ea9b4aae82b9764518eae014
n33alawar <nourhamza1987@gmail.com>
2019-11-13 13:41:12 +0100

    first commit

---
+++ new1.js
@@ -0,0 +1,45 @@
+// // // if (a > b && a > c) {
+// // //   console.log(a);
+// // // } else if (b > a && b > c) {
+// // //   console.log(b);
+// // // } else {
+// // //   console.log(c);
+// // // } //
+// let py = "py";
+// let string = "sport";
+// if (string[0].toLowerCase() === "p" && string[1].toLowerCase() === "y") {
+//   console.log(string);
+// } else {
+//   console.log(string.concat);
+// }
+// // // x = 2 ;
+// // // y = 10;
+// // // if
+// // // var x = Math.max(100, 10, 50, 1000.5, -99, -120, -100, 5);
+// // // console.log(Math.floor(x) + 10.2);
+// // // var x = Math.random();
+// // // myEndpattern = 1000;
+// // // console.log(Math.floor(Math.random() * myEndPattern + 1)); this code myEndPattern dont work we put this code to make nummmber limit
+// // let a = 1000;
+// // let b = 210;
+// // let c = 30;
+// // if (a > b && a > c) {
+// //   console.log(a);
+// // }
+// // if (b > a && b > c) {
+// //   console.log(b);
+// // }
+// // if (c > a && c > b) {
+// //   console.log(c);
+// // }
+//let num1 = 2.75; //espresso
+//let num2 = 2; // coffee
+//if (num1 % 1 === 0) {
+// console.log("coffee");
+//}
+let name = "nizar";
+let age = 39;
+let isApproved = true;
+let firstName = undefined;
+let selectedColor = red;
+console.log(name, age, isApproved, firstName, selectedColor);
